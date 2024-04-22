eb237ca64b455df5b73e47c01db33783d650f374
alvarocotes <alvaro.cotes@utp.edu.co>
2024-04-22 00:34:17 -0500

    se añanden comentarios ala clase PaymentFactory.java

--- src/PaymentFactory.java
+++ src/PaymentFactory.java
@@ -1,10 +1,11 @@
 public class PaymentFactory {
     public PaymentMethod createPayment(String paymentType) {
-        if ("cash".equalsIgnoreCase(paymentType)) {
-            return new CashPayment();
-        } else if ("credit".equalsIgnoreCase(paymentType)) {
-            return new CreditCardPayment();
-        } else {
+        if ("cash".equalsIgnoreCase(paymentType)) { // evalua si nuestra variable es igual a cash
+            return new CashPayment(); // si es igua nos da una istancia de la clase cashPayment
+        } else if ("credit".equalsIgnoreCase(paymentType)) { // evalua si nuestra variable es igual a credit
+            return new CreditCardPayment(); // si es igua nos da una istancia de la clase CreditCardPayment
+        } else { // en caso de que no decidamos que incluiremos mas pagos ps solo nos devolvera que no tenemos
+                // mas formas de pago
             throw new IllegalArgumentException("Tipo de pago no válido.");
         }
     }
